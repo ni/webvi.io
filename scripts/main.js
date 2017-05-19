1c110afd014a1468db83877cd380bf0810e277c8
Mark Black <mark.prestwick@gmail.com>
2017-05-18 21:21:37 -0500

    Fixed gauge sizing on ff, safari

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
-document.querySelector("jqx-tank .jqx-track").style.width="79px"
+// document.querySelector("jqx-tank .jqx-track").style.width="79px"
+
+(function () {
+    'use strict';
+
+    var niWebAppElement = document.querySelector('ni-web-application');
+
+    niWebAppElement.addEventListener('service-state-changed', function () {
+      if (niWebAppElement.serviceState.toLowerCase() === 'running') {
+        var tankTrack = document.querySelector('jqx-tank .jqx-track');
+        if (tankTrack === null) {
+            console.log("can't find tank");
+            return;
+        }
+        tankTrack.style.width="79px"
+
+        var gauge = document.querySelector('jqx-gauge');
+        if (gauge === null) {
+          console.log("can't find gauge");
+          return;
+        }
+        // gauge.style.position="absolute";
+        // gauge.style.position="relative";
+      }
+    })
+}());
